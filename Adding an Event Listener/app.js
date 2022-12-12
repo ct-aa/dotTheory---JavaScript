40e0240a219f400ef1139b4efc6168ceddb9334b
Alex Alfred Villanueva <alex.alfred.villanueva@codeandtheory.com>
2022-12-12 11:43:24 +0800

    ✨ JS-609: Adding an Event Listener

---
+++ Adding an Event Listener/app.js
@@ -0,0 +1,6 @@
+var warning = document.getElementById("warning");
+var button = document.getElementById("makeItRed");
+
+button.addEventListener('click', () => {
+    warning.style.backgroundColor = "red";
+});
