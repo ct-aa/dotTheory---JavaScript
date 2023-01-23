50eab4a668cd5e6371acfb254fcbc36511e520a4
Alex Alfred Villanueva <alex.alfred.villanueva@codeandtheory.com>
2023-01-23 10:15:14 +0800

    🐛 JS-608: Updated the html file

--- Return Elements Using CSS Selectors/app.js
+++ Return Elements Using CSS Selectors/app.js
@@ -1,3 +1,7 @@
 let navigationLinks = document.querySelectorAll('nav a');
 let galleryLinks = document.querySelectorAll('#gallery a');
 let footerImages = document.querySelectorAll('footer img');
+
+console.log(navigationLinks);
+console.log(galleryLinks);
+console.log(footerImages);
