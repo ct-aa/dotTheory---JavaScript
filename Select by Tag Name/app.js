6e73e4774c5af170f1bb629dd71d32515ee83a6f
Alex Alfred Villanueva <alex.alfred.villanueva@codeandtheory.com>
2023-01-23 16:07:53 +0800

    🐛 JS-608: Code clean up

--- Select by Tag Name/app.js
+++ Select by Tag Name/app.js
@@ -1,6 +1,6 @@
 // Complete the challenge by writing JavaScript below
 const colors = ["#C2272D", "#F8931F", "#FFFF01", "#009245", "#0193D9", "#0C04ED", "#612F90"];
-let listItems = document.getElementsByTagName('li');
+const listItems = document.getElementsByTagName('li');
 
 for (let i = 0; i < colors.length; i++) {
     listItems[i].style.color = colors[i];
