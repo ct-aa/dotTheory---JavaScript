012a16ae6dd3ca1fb56f4e5bde13d09c67d36d4b
Alex Alfred Villanueva <alex.alfred.villanueva@codeandtheory.com>
2023-01-23 09:41:46 +0800

    🐛 JS-610: Updated var to const

--- Finish the AJAX Request/app.js
+++ Finish the AJAX Request/app.js
@@ -1,4 +1,4 @@
-var request = new XMLHttpRequest();
+const request = new XMLHttpRequest();
 request.onreadystatechange = function () {
     if (request.readyState === 4) {
         document.getElementById("footer").innerHTML = request.responseText;
