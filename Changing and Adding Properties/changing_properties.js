1303f63af304f2daf1bf1daa3ceabd5637a2c16c
Alex Alfred Villanueva <alex.alfred.villanueva@codeandtheory.com>
2023-02-17 17:38:56 +0800

    🐛 JS-612: Created a play function

--- Changing and Adding Properties/changing_properties.js
+++ Changing and Adding Properties/changing_properties.js
@@ -1,24 +1,29 @@
+function play(name, isTurn) {
+	if (isTurn) {
+		console.log(`${name} is now playing!`);
+	}
+}
+
 const player1 = {
 	name: 'Ashley',
 	color: 'purple',
 	isTurn: true,
-	play: function () {
-		if (this.isTurn) {
-			console.log(`${this.name} is now playing!`);
-		}
-	}
+	play() {
+		play(this.name, this.isTurn);
+	},
 };
 
 const player2 = {
 	name: 'Guil',
 	color: 'red',
 	isTurn: false,
-	play: function () {
-		if (this['isTurn']) {
-			console.log(`${this['name']} is now playing!`);
-		}
-	}
+	play() {
+		play(this.name, this['isTurn']);
+	},
 };
 
-player1.isTurn = false;
+player1.isTurn = true;
 player2['isTurn'] = true;
+
+player1.play();
+player2.play();
