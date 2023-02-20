14c92df87cbe937fca0cab21f75419a77e67804d
Alex Alfred Villanueva <alex.alfred.villanueva@codeandtheory.com>
2023-02-20 10:08:21 +0800

    🐛 JS-609: Code cleanup

--- Event Delegation/app.js
+++ Event Delegation/app.js
@@ -1,9 +1,7 @@
-let inputItems = document.querySelectorAll('input');
+const inputItems = document.querySelectorAll('input');
 
 for (let i = 0; i < inputItems.length; i++) {
 	inputItems[i].addEventListener('click', (e) => {
-		if (e.target.tagName === 'INPUT') {
-			e.target.style.backgroundColor = 'rgb(255, 255, 0)';
-		}
+		e.target.style.backgroundColor = 'rgb(255, 255, 0)';
 	});
 }
