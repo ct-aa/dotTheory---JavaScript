afdbbca6d4277c2f1b54fc8a576196a0331bfdff
Alex Alfred Villanueva <alex.alfred.villanueva@codeandtheory.com>
2023-03-30 13:25:28 +0800

    🐛 JS-612: Code clean up

--- Changing and Adding Properties/changing_properties.js
+++ Changing and Adding Properties/changing_properties.js
@@ -1,29 +1,25 @@
 function play(name, isTurn) {
-	if (isTurn) {
-		console.log(`${name} is now playing!`);
-	}
+  if (isTurn) {
+    console.log(`${name} is now playing!`);
+  }
 }
 
 const player1 = {
-	name: 'Ashley',
-	color: 'purple',
-	isTurn: true,
-	play() {
-		play(this.name, this.isTurn);
-	},
+  name: 'Ashley',
+  color: 'purple',
+  isTurn: true,
+  play,
 };
 
 const player2 = {
-	name: 'Guil',
-	color: 'red',
-	isTurn: false,
-	play() {
-		play(this.name, this['isTurn']);
-	},
+  name: 'Guil',
+  color: 'red',
+  isTurn: false,
+  play,
 };
 
 player1.isTurn = true;
 player2['isTurn'] = true;
 
-player1.play();
-player2.play();
+player1.play(player1.name, player1.isTurn);
+player2.play(player2.name, player2.isTurn);
